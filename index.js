d8242d4bcd366584f903126cf0b39db2f0f08809
Brady Lumsden <blumsden007@gmail.com>
2022-04-13 11:10:28 -0700

    use express and a public folder

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 //jshint esversion: 6
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function(){
   console.log("server started and running on port 3000");
